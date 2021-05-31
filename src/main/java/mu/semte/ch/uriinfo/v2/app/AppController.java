307f5fbe0500b7dee5c86d3638a3e49b5f2fbfda
Nordine Bittich <nordine@bittich.be>
2021-05-31 09:51:57 +0200

    add controller

--- src/main/java/mu/semte/ch/uriinfo/v2/app/AppController.java
+++ src/main/java/mu/semte/ch/uriinfo/v2/app/AppController.java
@@ -1,27 +1,44 @@
 package mu.semte.ch.uriinfo.v2.app;
 
 import lombok.extern.slf4j.Slf4j;
+import mu.semte.ch.uriinfo.v2.app.dto.FrontendUI;
+import mu.semte.ch.uriinfo.v2.app.service.UIBuilderService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
 public class AppController {
+  private final UIBuilderService uiBuilderService;
 
- /*   @GetMapping(value = "/model",
-            produces = CONTENT_TYPE_JSON_LD)
-    public ResponseEntity<String> model(@RequestParam("uri") String uri) {
-        return ResponseEntity.badRequest().build();
-    }
+  public AppController(UIBuilderService uiBuilderService) {
+    this.uiBuilderService = uiBuilderService;
+  }
 
-    @GetMapping(value = "/meta",
-            produces = CONTENT_TYPE_JSON_LD)
-    public ResponseEntity<String> metaModel(@RequestParam("uri") String uri) {
-        return ResponseEntity.badRequest().build();
-    }
+  /*   @GetMapping(value = "/model",
+             produces = CONTENT_TYPE_JSON_LD)
+     public ResponseEntity<String> model(@RequestParam("uri") String uri) {
+         return ResponseEntity.badRequest().build();
+     }
 
-    @GetMapping("/page")
-    public ResponseEntity<FrontendUI> page(@RequestParam("uri") String uri) {
-        return ResponseEntity.badRequest().build();
-    }*/
+     @GetMapping(value = "/meta",
+             produces = CONTENT_TYPE_JSON_LD)
+     public ResponseEntity<String> metaModel(@RequestParam("uri") String uri) {
+         return ResponseEntity.badRequest().build();
+     }
 
+     @GetMapping("/page")
+     public ResponseEntity<FrontendUI> page(@RequestParam("uri") String uri) {
+         return ResponseEntity.badRequest().build();
+     }*/
+  @GetMapping("/page")
+  public ResponseEntity<FrontendUI> page(@RequestParam(value = "uri") String uri,
+                                         @RequestParam(value = "page",
+                                                       required = false) String pageUri) {
+
+    FrontendUI ui = uiBuilderService.build(uri, pageUri);
+    return ResponseEntity.ok(ui);
+  }
 }
