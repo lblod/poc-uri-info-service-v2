9f97916f8156019eead744f045494c4a196673eb
Nordine Bittich <nordine@bittich.be>
2021-06-01 09:49:04 +0200

    form

--- src/main/java/mu/semte/ch/uriinfo/v2/app/AppController.java
+++ src/main/java/mu/semte/ch/uriinfo/v2/app/AppController.java
@@ -2,6 +2,7 @@ package mu.semte.ch.uriinfo.v2.app;
 
 import lombok.extern.slf4j.Slf4j;
 import mu.semte.ch.uriinfo.v2.app.dto.FrontendUI;
+import mu.semte.ch.uriinfo.v2.app.dto.form.FrontendForm;
 import mu.semte.ch.uriinfo.v2.app.service.UIBuilderService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -39,6 +40,13 @@ public class AppController {
                                                        required = false) String pageUri) {
 
     FrontendUI ui = uiBuilderService.build(uri, pageUri);
+    return ResponseEntity.ok(ui);
+  }
+  @GetMapping("/form")
+  public ResponseEntity<FrontendForm> form(@RequestParam(value = "uri") String uri,
+                                         @RequestParam(value = "form") String formUri) {
+
+    FrontendForm ui = uiBuilderService.buildForm(uri, formUri);
     return ResponseEntity.ok(ui);
   }
 }
